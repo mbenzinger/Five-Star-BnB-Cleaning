e048f2360f82de2f93dbd78232910784730d137d
Mason Benzinger <mason.benzinger@gmail.com>
2022-12-11 14:54:00 -0600

    resolved conflicts

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const router = express.Router();
+const { Sequelize } = require('sequelize');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 const app = express()
+const pool = require('./config/config.js');
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+require('dotenv').config();
 
 // serve static frontend in production mode
 if (process.env.NODE_ENV === "production") {
