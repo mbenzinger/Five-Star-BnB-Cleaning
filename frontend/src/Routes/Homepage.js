d7e25909bc92d85b049ff4e2e553519b8398260f
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-12-12 17:45:56 -0600

    homepage edit

--- frontend/src/Routes/Homepage.js
+++ frontend/src/Routes/Homepage.js
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Homepage() {
   return (
@@ -33,7 +35,38 @@ function Homepage() {
           </Container>
         </Navbar>
       ))}
+
+<Container>
+      <Row>
+        <Col>
+        <h2>About Us</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and
+           typesetting industry. Lorem Ipsum has been the industry's 
+           standard dummy text ever since the 1500s, when an unknown 
+           printer took a galley of type and scrambled it to make a type 
+           specimen book. It has survived not only five centuries, but also 
+           the leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets 
+            containing Lorem Ipsum passages, and more recently with desktop publishing 
+            software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col>
+        <h2>Video</h2>
+        <iframe width="360" height="215" src="https://www.youtube.com/embed/OwRTIVKKERE" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+        picture-in-picture" allowfullscreen></iframe>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col><h2>How We Work</h2></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+
     </>
+    
   );
 }
 export default Homepage
