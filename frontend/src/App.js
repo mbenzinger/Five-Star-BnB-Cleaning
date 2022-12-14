a0b6abb6ca3e1f54193d1fb130d552eac875494b
Wade Graves <wadegraves940@gmail.com>
2022-12-13 19:49:07 -0500

    changes made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,28 +6,7 @@ import PropertyIndex from './properties/PropertyIndex';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <header>
-          <h1 className="title">Welcome to properties!</h1>
-
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
-
-        <div className="display">
-          <Route path="/" component={PropertyIndex} />
-          <Route path="/about" component={NewPropertyForm} />
-        </div>
-
-      </Router>
+      <NewPropertyForm />
 
     </div>
   );
