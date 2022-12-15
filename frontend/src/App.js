6f786e6c6ba7815bd1b8cc8e387bdb2156a065ce
Mason Benzinger <mason.benzinger@gmail.com>
2022-12-14 20:54:12 -0600

    created property add/edit/index routes, updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import AddPropertyForm from './Routes/AddPropertyForm';
+import PropertyDetails from './Routes/PropertyDetails';
+import EditPropertyForm from './Routes/EditPropertyForm';
 import LoginForm from './Routes/LoginForm';
 import SignUpForm from './Routes/SignUpForm';
 import Homepage from './Routes/Homepage';
@@ -14,7 +16,10 @@ function App() {
         <div>
           <Routes>
             <Route path="/" element={<Homepage />} />
-            <Route path="/Property" element={<AddPropertyForm />} />
+            <Route exact path="/places" element={<PlaceIndex />} />
+            <Route exact path="/places/new" element={<AddPropertyForm />} />
+            <Route exact path="/places/:placeId" element={<PropertyDetails />} />
+            <Route exact path="/places/:placeId/edit" element={<EditPropertyForm />} />
             <Route path="/signup" element={<SignUpForm />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/Error404" element={<Error404 />} />
